57fea90c16f3e1b67135b0408621eb909445632d
x0446891 <fabriciosouza@santos.sp.gov.br>
2022-12-28 12:06:20 -0300

    adicionando a rota index de teste

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send("Its working!");
+})
+
 app.listen(3333, ()=> {
     console.log('HTTP server schedule-man running on port 3333!');
 });
